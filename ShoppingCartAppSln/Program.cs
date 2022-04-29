d9043c6096c22e5fc148fc2684ce4eed7f88a4b3
AqeelaGriffiths <102726835+AqeelaGriffiths@users.noreply.github.com>
2022-04-29 15:43:09 +0200

    Add project files.

---
+++ ShoppingCartAppSln/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using shoppinglibrary;
+
+namespace shoppinglibrary
+
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            ShoppingCart cart = new ShoppingCart();
+
+            var item = new ShoppingCartItem("Headphones",200);
+            cart.Addshoppingcartitem(item);
+
+            item = new ShoppingCartItem("GamingChair",300);
+            cart.Addshoppingcartitem(item);
+
+            item = new ShoppingCartItem("laptop", 400);
+            cart.Addshoppingcartitem(item);
+
+            item = new ShoppingCartItem("ipad", 1300);
+            cart.Addshoppingcartitem(item);
+
+            item = new ShoppingCartItem("xbox", 2900);
+            cart.Addshoppingcartitem(item);
+
+            List<ShoppingCartItem> shoppingCartItems = cart.ShoppingCartItems();
+            foreach (ShoppingCartItem shoppingCartItem in shoppingCartItems) 
+            {
+                Console.WriteLine(shoppingCartItem.Name + "\t\t\t" + shoppingCartItem.Price);
+            }
+            Console.WriteLine("subTotal" + "\t" + cart.GetsubTotal());
+            Console.WriteLine("With Vat" + "\t" + cart.Getwithvattotal());
+        }
+    }
+
+}
---
+++ ShoppingCartAppSln/ShoppingCart.cs
@@ -0,0 +1,6 @@
+﻿namespace ShoppingCartItem
+{
+    internal class ShoppingCart
+    {
+    }
+}
